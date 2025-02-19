1d0c607deefb94f3e17ec003d62bd094583ffb7d
Evandro Santos Afonso <evandrosantosafonso@live.com>
2025-02-18 21:35:44 -0300

    adicionando a função de aplicar desconto

--- Produto.py
+++ Produto.py
@@ -6,6 +6,9 @@ class Produto:
         self.preco = preco
 
         Produto.produtos[self.nome] = {"nome": self.nome, "preco": self.preco}
+
+    def aplicar_desconto (self,desconto):
+        self.preco -= (desconto/100) * self.preco
     
     def __str__(self):
         return f"Produto: {self.nome} Preço : {self.preco}"
